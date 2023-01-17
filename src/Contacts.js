841f0cfba3b29c38bd15ecb0aa162880e205b916
Sayyid Sajad <111043430+sa7ad@users.noreply.github.com>
2023-01-17 14:19:20 +0530

    first commit

---
+++ src/Contacts.js
@@ -0,0 +1,29 @@
+const contacts = [
+    {
+      id:1,
+      name: "Muhammed Minhaj",
+      imgURL:
+        "https://i.postimg.cc/L41z2NN9/Whats-App-Image-2023-01-17-at-14-14-58.jpg",
+      phone: "+123 456 789",
+      email: "minhaj123@gmail.com"
+    },
+    {
+      id:2,
+      name: "Shanid Pilakkadan",
+      imgURL:
+        "https://i.postimg.cc/jSW86sZK/Whats-App-Image-2023-01-17-at-14-14-01.jpg",
+      phone: "+987 654 321",
+      email: "shanidpk@gmail.com"
+    },
+    {
+      id:3,
+      name: "Amal Arshad",
+      imgURL:
+        "https://i.postimg.cc/hjxfW3gN/Whats-App-Image-2023-01-17-at-14-15-55.jpg",
+      phone: "+918 372 574",
+      email: "amalpk@gmail.com"
+    }
+  ];
+
+  export default contacts;
+  
